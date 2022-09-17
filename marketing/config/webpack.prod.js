dce2211feadfe0df65f9c4fc4afa54f0aa2735a5
Vishal <trickyjsprogram@gmail.com>
2022-09-18 00:06:38 +0530

    added container file

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFedrationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commongConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFedrationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commongConfig, prodConfig);
